d59eab732604985c302bcbc43bab4c8f0b6bed8a
chelaru.artur <chelaru.artur@mail.ru>
2021-05-13 17:21:58 +0300

    basic views

--- app/js/app.js
+++ app/js/app.js
@@ -6,9 +6,39 @@ var app = angular.module('myApp', ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-      templateUrl : "pages/main.html"
+      redirectTo: 'main'
+    })
+    .when("/main", {
+      templateUrl : "pages/main.html",
+      controller: 'mainController'
     })
     .when("/login", {
-      templateUrl : "pages/login.html"
+      templateUrl : "pages/login.html",
+      controller: 'loginController'
     })
   });
+
+  app.controller('loginController', function($scope) {
+
+  });
+
+
+  app.controller('mainController', function($scope) {
+    $scope.data = [];
+    $scope.showModal = false;
+    $scope.selectedItem = null;
+    fetch('data/data.json').then(res => res.json()).then(res => {
+      console.log(res);
+      $scope.data = res;
+      $scope.$apply();
+    });
+    $scope.showDetails = function(item) {
+      console.log(item);
+      $scope.showModal = true;
+      $scope.selectedItem = item;
+    }
+
+    $scope.closeModal = function(){
+      $scope.showModal = false;
+    };
+  })
